caaee517a637ef755892d59ab7d285d6a42b679b
Jef Driesen <jef@libdivecomputer.org>
2023-04-28 13:37:44 +0200

    Keep the bluetooth logging enabled

--- core/qt-ble.cpp
+++ core/qt-ble.cpp
@@ -718,10 +718,12 @@ dc_status_t qt_ble_close(void *io)
 }
 static void checkThreshold()
 {
+#if 0
 	if (++debugCounter == DEBUG_THRESHOLD) {
 		QLoggingCategory::setFilterRules(QStringLiteral("qt.bluetooth* = false"));
 		report_info("turning off further BT debug output");
 	}
+#endif
 }
 
 /*
