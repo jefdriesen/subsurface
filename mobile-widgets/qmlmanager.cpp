f5e19496743297bb9f53038322f086ec57c74d41
Jef Driesen <jef@libdivecomputer.org>
2024-12-20 10:16:29 +0100

    Add timestamps to the mobile log

--- mobile-widgets/qmlmanager.cpp
+++ mobile-widgets/qmlmanager.cpp
@@ -59,6 +59,8 @@
 #include "commands/command_base.h"
 #include "commands/command.h"
 
+#include <sys/time.h>
+
 #if defined(Q_OS_ANDROID)
 #include <QJniObject>
 #include <QCoreApplication>
@@ -1934,7 +1936,14 @@ void writeToAppLogFile(const std::string &logText)
 void QMLManager::writeToAppLogFile(const std::string &logText)
 {
 	if (appLogFileOpen) {
-		std::string line = logText + "\n";
+		struct timeval now = {0};
+		gettimeofday (&now, NULL);
+
+		char prefix[32] = {0};
+		snprintf (prefix, sizeof(prefix), "[%li.%06li] ",
+			now.tv_sec, now.tv_usec);
+
+		std::string line = prefix + logText + "\n";
 		appLogFile.write(line.c_str());
 		appLogFile.flush();
 	}
